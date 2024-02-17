343d11b970c506011864a722ba58882e1b23b089
uurhaa <uurha.dev@gmail.com>
2024-02-17 07:59:42 +0100

    Initial implementation

---
+++ Assets/BetterSingletons/Runtime/MonoSingleton.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Better.Extensions.Runtime;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Better.Singletons.Runtime
+{
+    public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
+    {
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                ValidateInstance();
+                return _instance;
+            }
+        }
+
+        private static void ValidateInstance()
+        {
+            var typeName = typeof(T).Name;
+            if (_instance != null)
+            {
+                return;
+            }
+
+            var objects = FindObjectsOfType<T>();
+
+            if (objects.IsNullOrEmpty())
+            {
+                throw new InvalidOperationException($"[{typeName}] {nameof(ValidateInstance)}: no instance found");
+            }
+
+            _instance = objects[0];
+
+            if (objects.Length > 1)
+            {
+                Debug.LogWarning($"[{typeName}] {nameof(ValidateInstance)}: more than one instance found. Destroying other instances");
+
+                var others = new List<T>(objects);
+                others.Remove(_instance);
+
+                //TODO: Maybe add settings to destroy or reassign instance
+                foreach (var item in others)
+                {
+                    Destroy(item.gameObject);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/BetterSingletons/Runtime/ScriptableSingletonAsset.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Better.Extensions.Runtime;
+using UnityEditor;
+using UnityEngine;
+
+namespace Better.Singletons.Runtime
+{
+    public abstract class ScriptableSingletonAsset<T> : ScriptableObject where T : ScriptableSingletonAsset<T>
+    {
+#if UNITY_EDITOR
+
+        private static T _assetInstance;
+
+        public static T AssetInstance
+        {
+            get
+            {
+                if (_assetInstance == null)
+                {
+                    _assetInstance = FindAsset();
+                }
+
+                return _assetInstance;
+            }
+        }
+
+        private static T FindAsset()
+        {
+            var typeName = typeof(T).Name;
+            var filter = $"t:{typeName}";
+            var guids = AssetDatabase.FindAssets(filter);
+
+            if (guids.IsNullOrEmpty())
+            {
+                throw new InvalidOperationException($"[{typeName}] {nameof(FindAsset)}: no asset found");
+            }
+
+            if (guids.Length > 1)
+            {
+                Debug.LogWarning($"[{typeName}] {nameof(FindAsset)}: more than one asset found");
+            }
+
+            //TODO: Add BetterResources or AssetDatabaseUtility
+            var assetPath = AssetDatabase.GUIDToAssetPath(guids[0]);
+            return AssetDatabase.LoadAssetAtPath<T>(assetPath);
+        }
+
+#endif
+    }
+}
