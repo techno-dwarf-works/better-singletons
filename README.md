58386152e5d82fc1dca251111be1a1a12f77c64e
uurha <uurha@users.noreply.github.com>
2024-02-17 21:23:39 +0000

    Update README.md and LICENSE

---
+++ README.md
@@ -0,0 +1,4 @@
+# Better Singletons
+
+## Install
+[How to install](https://github.com/uurha/BetterPluginCollection/wiki/How-to-install)
---
+++ Runtime/PocoSingleton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Better.Singletons.Runtime
+{
+    [Serializable]
+    public abstract class PocoSingleton<T> where T : PocoSingleton<T>, new()
+    {
+        private static T _instance;
+        
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new T();
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
